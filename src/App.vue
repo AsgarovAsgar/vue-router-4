14918547308811e30cc659838494cbde2bcbb406
Asgarov Asgar <asgara@maestro.st>
2023-10-12 23:07:52 +0400

    embellishments

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
   <TheNavigation />
   <div class="container">
-    <RouterView :key="$route.path" />
+    <RouterView v-slot="{Component}">
+      <transition name="slide" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </RouterView>
   </div>
 </template>
 
 <script setup>
 import TheNavigation from '@/components/TheNavigation.vue';
 </script>
+
+<style lang="css">
+  .slide-enter-active,
+  .slide-leave-active {
+    transition: opacity 0.5s, transform 0.5s;
+  }
+
+  .slide-enter-from,
+  .slide-leave-to {
+    opacity: 0;
+    transform: translate(-30%)
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from '@/views/HomeView.vue'
+import sourceData from '@/data.json'
 
 const routes = [
   {
@@ -17,6 +18,17 @@ const routes = [
     name: 'destination.show',
     component: () => import('@/views/DestinationShowView.vue'),
     props: route => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter: (to, from) => {
+      const exists = sourceData.destinations
+        .find(destination => destination.id === parseInt(to.params.id))
+      if(!exists) return {
+        name: 'NotFound',
+        // always keep the url while rendering a different page
+        params: { pathMatch: to.path.split('/').slice(1) },
+        query: to.query,
+        hash: to.hash
+      }
+    },
     children: [
       {
         path: ':experienceSlug',
@@ -26,11 +38,22 @@ const routes = [
       }
     ]
   },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('@/views/NotFoundView.vue')
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || new Promise(resolve => {
+      setTimeout(() => resolve({ top: 0 }), 500) 
+      // should be the same with page transition duration
+    })
+  }
 });
 
 export default router;
