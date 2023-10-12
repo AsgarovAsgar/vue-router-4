cde73662e7deaa6a41ec4a2a301e3a4d1e47aa44
Asgarov Asgar <asgara@maestro.st>
2023-10-12 23:45:27 +0400

    guarded routes

---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login">
+    <form class="form" @submit.prevent="handleSubmit">
+      <h1>Login</h1>
+      <label for="username">Username</label>
+      <input v-model="form.username" name="username" type="text" class="input">
+      <label for="password">Password</label>
+      <input v-model="form.password" name="password" type="password" class="input">
+      <button class="btn">Login</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter()
+
+const form = ref({
+  username: '',
+  password: ''
+})
+
+const handleSubmit = () => {
+  window.user = form.value.username
+  router.push({ name: 'protected' })
+}
+</script>
---
+++ src/views/ProtectedView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Welcome, {{ username }}</h1>
+    <button @click="handleLogout">Log Out</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter()
+
+const username = ref(window.user)
+
+const handleLogout = () => {
+  window.user = null
+  router.push({ name: 'home' })
+}
+</script>
